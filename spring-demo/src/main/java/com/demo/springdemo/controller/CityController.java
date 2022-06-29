2f4e76fac2c6518a52657b824ca191c314f5eb62
MIRELA-CR <rusu.mirela444@gmail.com>
2022-06-29 21:41:50 +0300

    CURS3 TEMA

--- spring-demo/src/main/java/com/demo/springdemo/controller/CityController.java
+++ spring-demo/src/main/java/com/demo/springdemo/controller/CityController.java
@@ -18,7 +18,18 @@ public class CityController {
                 new City("Los Angeles","USA", 219),
                 new City("Seoul","South Korea", 201),
                 new City("London","UK", 192),
+                new City("Tokyo","Japan",173),
+                new City("Chengdu", "China",150),
+                new City("Amsterdam","The Netherlands",144),
+                new City("New York City", "USA",140),
+                new City("San Francisco","USA",132),
+                new City("Taipei", "Taiwan",131),
+                new City("Shanghai","China",120),
+                new City("Brussels", "Belgium",93),
+                new City("Milan","Italy",90),
+                new City("Stockholm","Sweden",90),
                 new City("Washington DC","USA", 74),
+                new City("Bucharest", "Romania", 60),
                 new City("Beijing","China", 51)
         );
         model.addAttribute("cityList", cityList);
